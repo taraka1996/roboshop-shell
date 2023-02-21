a6d7ce57ae5637d3ea4c0db0b2043e7cf38301fc
TARAK <tarakaramtirumala@gmail.com>
2023-02-21 11:28:45 +0530

    print in shell

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,20 @@
-echo -e "\e[35mINSTALLING NGINX\e[0m"
+echo -e "\e[35mINSTALLING NGINX \e[0m"
 yum install nginx -y
-echo -e "\e[35mremoving old content\e[0m"
+echo -e "\e[35mremoving old content \e[0m"
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[35mDOWNLOADING FRONTEND CONTENT\e[0m"
+echo -e "\e[35mDOWNLOADING FRONTEND CONTENT \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\e[35mEXTRACTING DOWNLOADED FRONTEND\e[0m"
+echo -e "\e[35mEXTRACTING DOWNLOADED FRONTEND \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-echo -e "\e[35mCOPYING NGINX CONFIG FOR ROBOSHOP\e[0m"
+echo -e "\e[35mCOPYING NGINX CONFIG FOR ROBOSHOP \e[0m"
 cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
-echo -e "\e[35mENABLING NGINX\e[0m"
+echo -e "\e[35mENABLING NGINX \e[0m"
 systemctl enable nginx
 
-echo -e "\e[35mSTARTING NGINX\e[0m"
+echo -e "\e[35mSTARTING NGINX \e[0m"
 systemctl restart nginx
