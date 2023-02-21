0b22bfd7976c930be64d46a971423d74cd7ee408
TARAK <tarakaramtirumala@gmail.com>
2023-02-21 11:55:36 +0530

    print in shell

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,5 @@
+code_dir=$ {pwd}
+
 echo -e "\e[35mINSTALLING NGINX \e[0m"
 yum install nginx -y
 echo -e "\e[35mremoving old content \e[0m"
@@ -11,7 +13,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 echo -e "\e[35mCOPYING NGINX CONFIG FOR ROBOSHOP \e[0m"
-cp /configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 echo -e "\e[35mENABLING NGINX \e[0m"
 systemctl enable nginx
