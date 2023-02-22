6dfa80ad03e2c4cd11a7a9a3fca8f5b124d25f15
TARAK <tarakaramtirumala@gmail.com>
2023-02-22 12:20:09 +0530

    changes

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 source common.sh
 
 print_head "Setup MongoDB repository"
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo >>${log_file}
+cp ${code_dir}/configs/mongod.repo /etc/yum.repos.d/mongod.repo >>${log_file}
 status_check $?
 
 
