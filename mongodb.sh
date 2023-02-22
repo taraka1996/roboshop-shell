083293d2fe93a30eea6a7563ab31b9cddbf71132
TARAK <tarakaramtirumala@gmail.com>
2023-02-22 14:40:36 +0530

    changes

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 source common.sh
 
 print_head "Setup MongoDB repository"
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
 status_check $?
 
 
@@ -10,7 +10,7 @@ yum install mongodb-org -y &>>${log_file}
 status_check $?
 
 print_head "Update MongoDB Listen address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf &>>${log_file}
 status_check $?
 
 print_head "Enable MongoDB"
