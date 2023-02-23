b0e7322ba62b4842bb09676bf33c91040d37e791
TARAK <tarakaramtirumala@gmail.com>
2023-02-23 10:17:39 +0530

    changes

--- user.sh
+++ user.sh
@@ -66,7 +66,7 @@ yum install mongodb-org-shell -y >>${log_file}
 status_check $?
 
 print_head  "load schema"
-mongo --host mongodb.devops-b-71.online /app/schema/catalogue.js &>>${log_file}
+mongo --host mongodb.devops-b-71.online /app/schema/user.js &>>${log_file}
 status_check $?
 
 
