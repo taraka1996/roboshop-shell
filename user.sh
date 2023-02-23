c58373967f929865092d8ae91706d1769f6cbced
TARAK <tarakaramtirumala@gmail.com>
2023-02-23 10:16:29 +0530

    changes

--- user.sh
+++ user.sh
@@ -1,70 +1,72 @@
-
-
 source common.sh
 
 print_head  "configure Nodejs repo"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash >>${log_file}
 status_check $?
 
 print_head  "Install Nodejs"
-yum install nodejs -y ${log_file}
+yum install nodejs -y >>${log_file}
 status_check $?
 
 print_head  "create roboshop user"
- id roboshop  ${log_file}
+ id roboshop  >>${log_file}
+
  if [ $? -ne 0 ]; then
-useradd roboshop ${log_file}
+useradd roboshop >>${log_file}
 fi
 status_check $?
 
 print_head  "create application directory"
 if [ ! -d /app ]; then
-mkdir /app ${log_file}
+mkdir /app >>${log_file}
 fi
 status_check $?
 
 print_head  "delete old content"
-rm -rf /app/* ${log_file}
+rm -rf /app/* >>${log_file}
 status_check $?
 
 print_head  "downloading app content"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip ${log_file}
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip >>${log_file}
 status_check $?
 cd /app
 
 print_head  "extracting app content"
-unzip /tmp/catalogue.zip ${log_file}
-cd /app ${log_file}
+unzip /tmp/user.zip >>${log_file}
+
+cd /app >>${log_file}
 status_check $?
 
 print_head  "installing Nodejs dependencies"
-npm install ${log_file}
+npm install >>${log_file}
 status_check $?
 
-print_head  "copy systemD service file"
-cp ${code_dir}/configs/user.service /etc/systemd/system/user.service ${log_file}
+print_head  "copy systemd service file"
+cp ${code_dir}/configs/user.service /etc/systemd/system/user.service &>>${log_file}
 status_check $?
 
-print_head  "reload systemD"
-systemctl daemon-reload ${log_file}
+print_head  "reload systemd"
+systemctl daemon-reload &>>${log_file}
 status_check $?
 
 print_head  "enable user  service"
-systemctl enable user ${log_file}
+systemctl enable user >>${log_file}
 status_check $?
 
 print_head  "start user  service"
-systemctl restart user ${log_file}
+systemctl restart user >>${log_file}
 status_check $?
 
 print_head  "copy mongodb repo file"
-cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo ${log_file}
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
 status_check $?
 
 print_head  "install mongo client"
-yum install mongodb-org-shell -y ${log_file}
+yum install mongodb-org-shell -y >>${log_file}
 status_check $?
 
 print_head  "load schema"
-mongo --host mongodb.devops-b-71.online </app/schema/catalogue.js ${log_file}
+mongo --host mongodb.devops-b-71.online /app/schema/catalogue.js &>>${log_file}
 status_check $?
+
+
